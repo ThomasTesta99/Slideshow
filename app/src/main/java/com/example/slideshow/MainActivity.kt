3b8c35a2ce5d097ab384cb37ff34c174843ad43c
ThomasTesta99 <ttesta99@yahoo.com>
2025-03-17 12:33:44 -0400

    Starting ImageBox

--- app/src/main/java/com/example/slideshow/MainActivity.kt
+++ app/src/main/java/com/example/slideshow/MainActivity.kt
@@ -4,13 +4,25 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.slideshow.ui.theme.SlideshowTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,10 +32,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             SlideshowTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+
                 }
             }
         }
@@ -31,17 +40,18 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun ImageBox(modifier: Modifier = Modifier){
+    var imageNum by remember { mutableStateOf(1) }
+    Box(){
+        Image(
+            painterResource(imageNum),
+            contentDescription = ""
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Row {
+            Button(onClick = {}){
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SlideshowTheme {
-        Greeting("Android")
+            }
+        }
     }
 }
